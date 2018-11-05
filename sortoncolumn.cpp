2015cb262884f7a591b0adde9708ae54d8915bbb
rakeshprasadyaday <rakeshprasadyaday@gmail.com>
2018-11-05 12:19:02 +0530

    Create sortoncolumn.cpp
    sorting 2D array on the basis of different no of column using vector.

---
+++ sortoncolumn.cpp
@@ -0,0 +1,49 @@
+// C++ code  2D Vector 
+// with different no. of columns 
+#include<iostream> 
+#include<vector> // for 2D vector 
+#include<algorithm>
+using namespace std; 
+
+bool sizecom(const vector<int>& v1, const vector<int>& v2)
+{
+    return v1.size()<v2.size();
+}
+
+//Driver Function
+
+int main() 
+{ 
+    //2D dynamic array
+	vector<vector<int>> myVector={{1,2},{3,4,5},{6}};
+	//Array befor sorting 
+	cout<<"The matrix before sorting\n";
+	for(int i=0;i<myVector.size();i++)
+	{
+	    for(int j=0;j<myVector[i].size();j++)
+	    {
+	        //Print the element of 2D array vector
+	        cout<<myVector[i][j]<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+//sort function for sorting vector on the basis of number of columns
+    sort(myVector.begin(),myVector.end(),sizecom);
+	
+	//Matrix after sorting the number of elements
+	cout<<"Matrix after sorting the number of elements\n";
+	for(int i=0;i<myVector.size();i++)
+    {
+        for(int j=0;j<myVector[i].size();j++)
+            {
+                cout<<myVector[i][j]<<" ";
+                
+            }
+            
+            cout<<endl;
+    }
+	
+	return 0; 
+
+} 
